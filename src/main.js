80226b980c258ebe21e3f44ef928dc80b4df6d29
Donald Moore <donald@maximumdev.biz>
2017-08-23 17:26:16 -0500

    Modularized

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
   $(document).on("scroll", function() {
     var pos_y = window.scrollY;
     console.log(pos_y);
-    if (pos_y >= 400) {
+    if (pos_y >= 1) {
       $('.r2').animate({
         opacity: 1,
         top: "0px"
       }, 1000, "easeInOutCubic");
     }
-    if (pos_y >= 1000) {
+    if (pos_y >= 900) {
       $('.r3').animate({
         opacity: 1,
         top: "0px"
