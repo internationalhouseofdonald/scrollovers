d373d16bd0dad43722f9cbb5922bdfbd9efa611e
Donald Moore <donald@maximumdev.biz>
2017-08-23 14:26:21 -0500

    Added Bootswatch theme Lumen

--- src/main.js
+++ src/main.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
   $(document).on("scroll", function() {
     var pos_y = window.scrollY;
     console.log(pos_y);
-    if (pos_y >= 300) {
+    if (pos_y >= 600) {
       $('.r2').animate({
         opacity: 1,
         top: "0px"
       }, 1000, "easeInOutCubic");
     }
-    if (pos_y >= 1000) {
+    if (pos_y >= 1400) {
       $('.r3').animate({
         opacity: 1,
         top: "0px"
